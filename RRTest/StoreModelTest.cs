74f08d1cc81030631c0b02e0667169e62396a811
ccwilki1 <ccwilki1@gmail.com>
2021-11-06 20:49:03 -0400

    changes made

--- RRTest/StoreModelTest.cs
+++ RRTest/StoreModelTest.cs
@@ -28,28 +28,28 @@ namespace RRTest
         
 
 //will test if property gives exception if given invalid data
-    [Theory]
-    [InlineData("dadada")]
-    [InlineData("freadad")]
-    [InlineData("dafafda")]
-    [InlineData("uapd")]
-    [InlineData("weraedf")]
-    [InlineData("babradfea")]
-    [InlineData("uarpd")]
-    [InlineData("werraedf")]
-    [InlineData("babardfea")]
-    [InlineData("weerraedf")]
-    [InlineData("baebardfea")]
-        public void CityShouldFailiWithInvalidData(string p_input)
+    // [Theory]
+    // [InlineData("dadada")]
+    // [InlineData("freadad")]
+    // [InlineData("dafafda")]
+    // [InlineData("uapd")]
+    // [InlineData("weraedf")]
+    // [InlineData("babradfea")]
+    // [InlineData("uarpd")]
+    // [InlineData("werraedf")]
+    // [InlineData("babardfea")]
+    // [InlineData("weerraedf")]
+    // [InlineData("baebardfea")]
+    //     public void CityShouldFailiWithInvalidData(string p_input)
         
-        {            
-            Customer _customerTest = new Customer();
+    //     {            
+    //         Customer _customerTest = new Customer();
 
-            //throw MethodAccessException will only pass if the code you did will give some sort of an exception.
+    //         //throw MethodAccessException will only pass if the code you did will give some sort of an exception.
             
-            Assert.Throws<Exception>(() => _customerTest.Name = p_input); 
+    //         Assert.Throws<Exception>(() => _customerTest.Name = p_input); 
 
 
-        }
+    //     }
     }
 }
